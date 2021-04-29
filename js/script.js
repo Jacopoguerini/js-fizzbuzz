9214c6312786a50660030839c145d534cc1d2e40
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-04-29 15:20:15 +0200

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -2,19 +2,20 @@
 ma per i multipli di 3 stampi "Fizz" al posto del numero e per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
 
+var multiplo = (i % 3 == 0) && (i % 5 == 0);
+
 for (var i = 1; i <= 100; i++) {
+    // multipli di 3 e 5
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+    console.log("FizzBuzz");
 
     // multipli di 3
-    if (i % 3 == 0) {
+    } else if (i % 3 == 0) {
         console.log("Fizz");
 
     // multipli di 5
     } else if (i % 5 == 0) {
         console.log("Buzz");
-
-    // multipli di 3 e 5
-    } else if ((i % 3 == 0) && (i % 5 == 0)) {
-        console.log("FizzBuzz");
 
     } else {
         console.log(i);
