32366d5528a51d4a5c5a82637fc68aa179f33ddb
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-04-29 18:32:40 +0200

    Eliminata riga inutile

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@
 ma per i multipli di 3 stampi "Fizz" al posto del numero e per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
 
-var multiplo = (i % 3 == 0) && (i % 5 == 0);
-
 for (var i = 1; i <= 100; i++) {
     // multipli di 3 e 5
     if ((i % 3 == 0) && (i % 5 == 0)) {
